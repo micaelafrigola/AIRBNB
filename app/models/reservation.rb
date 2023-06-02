9d07651d21040eae046e983a0a75dbdd52714ab6
Wai Ian Vong <valenvwi@gmail.com>
2023-06-02 14:38:05 +0200

    validation of occupancy and dates

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,12 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+
+
+  validates :start_date, comparison: {less_than: :end_date}
+  validate :check_maxi_guests
+
+  def check_maxi_guests
+    errors.add(:num_of_guests, "Can't exceed #{flat.occupancy} guests") if num_of_guests > flat.occupancy
+  end
 end
